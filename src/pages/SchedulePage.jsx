02b487dea218c99034a7f71609fd880d15dc044f
notnikhil07 <155284356+notnikhil07@users.noreply.github.com>
2025-03-28 13:31:32 +0530

    Update SchedulePage.jsx

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const SchedulePage = () => {
   return (
     <div className="min-h-[64vh] p-2 flex items-center justify-center flex-col gap-4 text-3xl text-blue-500">
-      <img src="../public/schedule.jpg" alt="" />
-      <img src="../public/schedule.jpg" alt="" />
-      <img src="../public/schedule.jpg" alt="" />
+      <img src="/schedule.jpg" alt="" />
+      <img src="/schedule.jpg" alt="" />
+      <img src="/schedule.jpg" alt="" />
     </div>
   );
 };
