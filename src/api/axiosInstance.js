81fdfb09b5f0408975c9bb354f7d8654c98664a3
notnikhil07 <155284356+notnikhil07@users.noreply.github.com>
2025-03-28 16:12:43 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://nexus25-production.up.railway.app/api",
+  baseURL: "https://nexus25.onrender.com/api",
   withCredentials: true,
 });
 
