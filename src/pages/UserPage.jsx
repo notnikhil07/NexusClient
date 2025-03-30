4e2f45c6b784b1849b815a4ee5e939dea1c5356c
notnikhil07 <155284356+notnikhil07@users.noreply.github.com>
2025-03-30 22:56:12 +0530

    Update UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -53,8 +53,8 @@ const UserPage = () => {
           <CiLogout /> Logout
         </p>
       </div>
-      <div className="my-2 mx-2 bg-green-500 text-black px-3 py-2 rounded flex items-center justify-center gap-1 cursor-pointer w-[300px]">
-        <FaWhatsapp className="text-3xl" />
+      <div className="my-2 mx-2 bg-green-500 text-black px-3 py-2 rounded flex items-center justify-center gap-1 cursor-pointer w-[230px]">
+        <FaWhatsapp className="text-2xl" />
         <a href="https://chat.whatsapp.com/Jvn8gGrBqvqHFXZHRlmYHu" target="_blank">Click to join Whatsapp Group</a>
       </div>
       {!userRegistration  ? (
